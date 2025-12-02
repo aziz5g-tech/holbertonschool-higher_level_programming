ceac60aedcd6b67767713a4bf739b5a38824b0fb
aziz5g-tech <bogico.sa@gmail.com>
2025-12-02 14:39:24 +0300

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
-# ABDULAZIZ ALRSHEDI <11937@holbertonschool.com>
+# Brennan D Baraban <375@holbertonschool.com>
 
 
 def roman_to_int(roman_string):
@@ -23,12 +23,10 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
-            
-            if (
-                i != (len(roman_string) - 1)
-                and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]
-            ):
-                num += roman_dict[roman_string[i]] * -1
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                    num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
