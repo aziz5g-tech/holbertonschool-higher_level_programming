c6c51067b02dae5e86fe935fbbe3ed2a2caeb801
aziz5g-tech <bogico.sa@gmail.com>
2025-12-02 14:22:02 +0300

    Create 5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+# ABDULAZIZ ALRSHEDI <11937@holbertonschool.com>
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
