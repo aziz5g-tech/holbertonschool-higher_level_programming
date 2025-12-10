90525c8f96e3959dad37332627045b4decfbf782
aziz5g-tech <bogico.sa@gmail.com>
2025-12-10 14:07:12 +0300

    Define Rectangle class in 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# ABDULAZIZ ALRSHEDI <11937@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
