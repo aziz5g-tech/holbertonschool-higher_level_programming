e93c149e823a212230c9941cd7d0c5eb9def7ca8
ABDULAZIZ ALRASHDI <bogico.sa@gmail.com>
2025-12-25 11:15:29 +0300

    Update 101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,63 +1,60 @@
 #!/usr/bin/python3
-"""module for use in log debugging
 """
-
-
+Script that reads stdin line by line and computes metrics
+"""
 import sys
 
 
-class DebugLog:
-    """class for use in debugging stdin logs
-    """
-    def __init__(self):
-        self.lines = []
-        self.total_size = 0
-        self.codes_200 = 0
-        self.codes_401 = 0
-        self.codes_403 = 0
-        self.codes_404 = 0
-        self.codes_405 = 0
-        self.codes_500 = 0
-        self.__current_line = 0
-
-    def __str__(self):
-        string = "File size: " + str(self.total_size) + '\n'
-        string += "200: " + str(self.codes_200) + '\n'
-        string += "401: " + str(self.codes_401) + '\n'
-        string += "403: " + str(self.codes_403) + '\n'
-        string += "404: " + str(self.codes_404) + '\n'
-        string += "405: " + str(self.codes_405) + '\n'
-        string += "500: " + str(self.codes_500)
-        return string
-
-    def update(self, iteration_n):
-        """updates current response numbers
-            with new set of lines
-        """
-        for i in range(self.__current_line, self.__current_line + iteration_n):
-            if "200" in self.lines[i]:
-                self.codes_200 += 1
-            elif "401" in self.lines[i]:
-                self.codes_401 += 1
-            elif "403" in self.lines[i]:
-                self.codes_403 += 1
-            elif "404" in self.lines[i]:
-                self.codes_404 += 1
-            elif "405" in self.lines[i]:
-                self.codes_405 += 1
-            elif "500" in self.lines[i]:
-                self.codes_500 += 1
-            words = line.split(' ')
-            self.total_size += int(words[-1])
-            self.__current_line += 1
+def print_stats(total_size, status_counts):
+    """Print the current statistics"""
+    print("File size: {}".format(total_size))
+    for status_code in sorted(status_counts.keys()):
+        if status_counts[status_code] > 0:
+            print("{}: {}".format(status_code, status_counts[status_code]))
 
 
-log = DebugLog()
-lines = []
-line = sys.stdin.readline()
-while line != "":
-    log.lines.append(line)
-    if len(log.lines) % 10 == 0:
-        log.update(10)
-        print(log)
-    line = sys.stdin.readline()
+def main():
+    """Main function to process log lines and compute metrics"""
+    total_size = 0
+    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            if not line:
+                continue
+
+            # Parse the line according to the expected format
+            try:
+                # Split by spaces and get the last two parts (status code and file size)
+                parts = line.split()
+                if len(parts) < 2:
+                    continue
+
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+
+                # Only count if status code is in our valid list
+                if status_code in status_counts:
+                    status_counts[status_code] += 1
+                    total_size += file_size
+                    line_count += 1
+
+                    # Print stats every 10 lines
+                    if line_count % 10 == 0:
+                        print_stats(total_size, status_counts)
+
+            except (ValueError, IndexError):
+                # Skip lines that don't match the expected format
+                continue
+
+    except KeyboardInterrupt:
+        pass
+
+    # Print final stats
+    print_stats(total_size, status_counts)
+
+
+if __name__ == "__main__":
+    main()
