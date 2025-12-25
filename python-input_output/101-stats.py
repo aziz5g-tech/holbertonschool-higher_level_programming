7d9a82d66b8db43f84eb7d230a463dcadb4f2824
ABDULAZIZ ALRASHDI <bogico.sa@gmail.com>
2025-12-25 11:22:41 +0300

    Update 101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -15,7 +15,8 @@ def print_stats(total_size, status_counts):
 
 if __name__ == "__main__":
     total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_counts = {200: 0, 301: 0, 400: 0,
+                     401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     line_count = 0
 
     try:
@@ -25,17 +26,18 @@ if __name__ == "__main__":
                 if len(parts) >= 2:
                     status_code = int(parts[-2])
                     file_size = int(parts[-1])
-                    
+
+                    total_size += file_size
+                    line_count += 1
+
                     if status_code in status_counts:
                         status_counts[status_code] += 1
-                        total_size += file_size
-                        line_count += 1
-                        
-                        if line_count % 10 == 0:
-                            print_stats(total_size, status_counts)
+
+                    if line_count % 10 == 0:
+                        print_stats(total_size, status_counts)
             except:
                 pass
     except KeyboardInterrupt:
         pass
-    
+
     print_stats(total_size, status_counts)
