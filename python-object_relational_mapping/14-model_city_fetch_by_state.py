32dee2c3dbfb8ba6dc7c0ef4f1f4e6bd8280d9bc
ABDULAZIZ ALRASHEDI <bogico.sa@gmail.com>
2026-01-17 08:41:48 +0300

    Fix pycodestyle: remove whitespace from blank lines in all remaining files

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     cities = session.query(City, State).join(State).order_by(City.id).all()
     for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    
+
     session.close()
