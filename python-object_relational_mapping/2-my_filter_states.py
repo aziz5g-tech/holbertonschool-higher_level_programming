a5e0a5a31465c898656cbf7679a210ce25590fdd
ABDULAZIZ ALRASHEDI <bogico.sa@gmail.com>
2026-01-17 08:29:13 +0300

    Fix pycodestyle: line too long in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that displays all values in the states table where name matches the argument"""
+"""Script that displays all values in the states table
+where name matches the argument"""
 import MySQLdb
 import sys
 
@@ -12,8 +13,8 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
-    cursor.execute(query)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cursor.execute(query.format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
